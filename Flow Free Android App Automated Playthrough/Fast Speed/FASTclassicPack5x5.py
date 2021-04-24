75d3221a1e7464f50a3626cdbfa795147e6a2a3b
ddrmslash <83101810+ddrmslash@users.noreply.github.com>
2021-04-23 20:46:06 -0500

    Add files via upload

---
+++ Flow Free Android App Automated Playthrough/Fast Speed/FASTclassicPack5x5.py
@@ -0,0 +1,338 @@
+from pynput.keyboard import Key, Controller as KeyboardController
+from pynput.mouse import Button, Controller as MouseController
+
+import time
+
+
+mouse = MouseController()
+
+# In the future would have to code in the path breaks
+
+time.sleep(1)
+#========================================================
+# CLASSIC PACK - FIRST 5 LEVELS
+#========================================================
+
+
+#========================================================
+# Level 1
+#=========================================================
+
+mouse.position = (1479, 375)
+mouse.press(Button.left)
+time.sleep(.05)
+mouse.position = (1482, 527)
+mouse.release(Button.left)
+mouse.press(Button.left)
+time.sleep(.05)
+mouse.position = (1472, 669)
+time.sleep(.05)
+mouse.position = (1567, 665)
+mouse.release(Button.left)
+
+time.sleep(.03)
+
+mouse.position = (1563, 584)
+mouse.press(Button.left)
+#time.sleep(2)
+mouse.position = (1554, 444) #(1565, 363)
+mouse.release(Button.left)
+mouse.press(Button.left)
+#time.sleep(1)
+mouse.position = (1628, 376)
+mouse.release(Button.left)
+
+time.sleep(.3)
+
+#blue
+mouse.position = (1636, 447)
+mouse.press(Button.left)
+#time.sleep(2)
+mouse.position = (1634, 670)
+mouse.release(Button.left)
+
+time.sleep(.3)
+
+mouse.position = (1717, 670)
+mouse.press(Button.left)
+#time.sleep(2)
+mouse.position = (1780, 665)
+mouse.release(Button.left)
+mouse.press(Button.left)
+#time.sleep(2)
+mouse.position = (1788, 442)
+mouse.release(Button.left)
+
+time.sleep(.3)
+
+mouse.position = (1781, 362)
+mouse.press(Button.left)
+#time.sleep(2)
+mouse.position = (1696, 363)
+mouse.release(Button.left)
+mouse.press(Button.left)
+#time.sleep(1)
+mouse.position = (1712, 585)
+mouse.release(Button.left)
+
+time.sleep(2)
+mouse.position = (1634, 539)
+mouse.press(Button.left)
+mouse.release(Button.left)
+
+time.sleep(1)
+#========================================================
+# Level 2
+#=========================================================
+
+mouse.position = (1506, 359)
+mouse.press(Button.left)
+#time.sleep(2)
+mouse.position = (1651, 362)
+mouse.release(Button.left)
+mouse.press(Button.left)
+#time.sleep(2)
+mouse.position = (1773, 367)
+mouse.release(Button.left)
+mouse.press(Button.left)
+#time.sleep(1)
+mouse.position = (1788, 606)
+mouse.release(Button.left)
+
+
+time.sleep(.3)
+
+mouse.position = (1778, 664)
+mouse.press(Button.left)
+#time.sleep(2)
+mouse.position = (1711, 665)
+mouse.release(Button.left)
+mouse.press(Button.left)
+#time.sleep(2)
+mouse.position = (1715, 437)
+mouse.release(Button.left)
+mouse.press(Button.left)
+#time.sleep(1)
+mouse.position = (1492, 435)
+mouse.release(Button.left)
+mouse.press(Button.left)
+mouse.position = (1486, 602)
+mouse.release(Button.left)
+
+time.sleep(.3)
+
+mouse.position = (1556, 596)
+mouse.press(Button.left)
+#time.sleep(2)
+mouse.position = (1626, 517)
+mouse.release(Button.left)
+
+
+time.sleep(.3)
+
+mouse.position = (1627, 605)
+mouse.press(Button.left)
+#time.sleep(2)
+mouse.position = (1629, 672)
+mouse.release(Button.left)
+mouse.press(Button.left)
+#time.sleep(1)
+mouse.position = (1501, 669)
+mouse.release(Button.left)
+
+time.sleep(2)
+mouse.position = (1634, 539)
+mouse.press(Button.left)
+mouse.release(Button.left)
+
+time.sleep(1)
+#========================================================
+# Level 3
+#=========================================================
+
+
+mouse.position = (1712, 372)
+mouse.press(Button.left)
+mouse.position = (1778, 372)
+mouse.release(Button.left)
+mouse.press(Button.left)
+mouse.position = (1786, 522)
+mouse.release(Button.left)
+mouse.press(Button.left)
+mouse.position = (1787, 663)
+mouse.release(Button.left)
+mouse.press(Button.left)
+mouse.position = (1708, 666)
+mouse.release(Button.left)
+
+time.sleep(.3)
+
+mouse.position = (1629, 660)
+mouse.press(Button.left)
+mouse.position = (1708, 593)
+mouse.release(Button.left)
+
+time.sleep(.3)
+
+mouse.position = (1642, 517)
+mouse.press(Button.left)
+mouse.position = (1709, 457)
+mouse.release(Button.left)
+time.sleep(.3)
+
+mouse.position = (1634, 384)
+mouse.press(Button.left)
+mouse.position = (1624, 444)
+mouse.release(Button.left)
+mouse.press(Button.left)
+mouse.position = (1569, 436)
+mouse.release(Button.left)
+mouse.press(Button.left)
+mouse.position = (1565, 583)
+mouse.release(Button.left)
+mouse.press(Button.left)
+mouse.position = (1496, 663)
+mouse.release(Button.left)
+
+time.sleep(.3)
+
+mouse.position = (1476, 592)
+mouse.press(Button.left)
+mouse.position = (1484, 425)
+mouse.release(Button.left)
+mouse.press(Button.left)
+mouse.position = (1562, 366)
+mouse.release(Button.left)
+
+
+time.sleep(2)
+mouse.position = (1634, 539)
+mouse.press(Button.left)
+mouse.release(Button.left)
+
+time.sleep(1)
+#========================================================
+# Level 4
+#=========================================================
+
+mouse.position = (1489, 442)
+mouse.press(Button.left)
+mouse.position = (1484, 364)
+mouse.release(Button.left)
+mouse.press(Button.left)
+mouse.position = (1706, 364)
+mouse.release(Button.left)
+
+time.sleep(.5)
+
+mouse.position = (1780, 369)
+mouse.press(Button.left)
+mouse.position = (1785, 441)
+mouse.release(Button.left)
+mouse.press(Button.left)
+mouse.position = (1632, 437)
+mouse.release(Button.left)
+mouse.press(Button.left)
+mouse.position = (1554, 430)
+mouse.release(Button.left)
+mouse.press(Button.left)
+mouse.position = (1557, 520)
+mouse.release(Button.left)
+mouse.press(Button.left)
+time.sleep(.5)
+mouse.position = (1491, 523)
+mouse.release(Button.left)
+mouse.press(Button.left)
+mouse.position = (1484, 672)
+mouse.release(Button.left)
+
+time.sleep(.5)
+
+mouse.position = (1561, 663)
+mouse.press(Button.left)
+mouse.position = (1572, 581)
+mouse.release(Button.left)
+mouse.press(Button.left)
+mouse.position = (1700, 580)
+mouse.release(Button.left)
+
+time.sleep(.3)
+
+
+mouse.position = (1632, 664)
+mouse.press(Button.left)
+mouse.position = (1772, 664)
+mouse.release(Button.left)
+mouse.press(Button.left)
+mouse.position = (1792, 531)
+mouse.release(Button.left)
+mouse.press(Button.left)
+mouse.position = (1643, 525)
+mouse.release(Button.left)
+
+
+
+time.sleep(2)
+mouse.position = (1634, 539)
+mouse.press(Button.left)
+mouse.release(Button.left)
+
+time.sleep(1)
+#========================================================
+# Level 5
+#=========================================================
+
+
+mouse.position = (1789, 370)
+mouse.press(Button.left)
+mouse.position = (1716, 451)
+mouse.release(Button.left)
+
+time.sleep(.3)
+
+mouse.position = (1711, 372)
+mouse.press(Button.left)
+mouse.position = (1473, 372)
+mouse.release(Button.left)
+mouse.press(Button.left)
+mouse.position = (1486, 516)
+mouse.release(Button.left)
+mouse.press(Button.left)
+mouse.position = (1485, 664)
+mouse.release(Button.left)
+mouse.press(Button.left)
+mouse.position = (1629, 665)
+mouse.release(Button.left)
+
+time.sleep(.3)
+
+mouse.position = (1787, 665)
+mouse.press(Button.left)
+mouse.position = (1702, 665)
+mouse.release(Button.left)
+mouse.press(Button.left)
+mouse.position = (1703, 599)
+mouse.release(Button.left)
+mouse.press(Button.left)
+mouse.position = (1564, 584)
+mouse.release(Button.left)
+mouse.press(Button.left)
+mouse.position = (1555, 450)
+mouse.release(Button.left)
+
+time.sleep(.3)
+
+mouse.position = (1644, 454)
+mouse.press(Button.left)
+mouse.position = (1639, 521)
+mouse.release(Button.left)
+mouse.press(Button.left)
+mouse.position = (1708, 521)
+mouse.release(Button.left)
+mouse.press(Button.left)
+mouse.position = (1785, 589)
+mouse.release(Button.left)
+
+
+time.sleep(2)
+mouse.position = (1634, 539)
---
+++ Flow Free Android App Automated Playthrough/MousePointerTool.py
@@ -0,0 +1,19 @@
+
+from pynput.keyboard import Key, Controller as KeyboardController
+from pynput.mouse import Button, Controller as MouseController
+
+import time
+
+mouse = MouseController()
+
+time.sleep(3)
+
+while True:
+    print('mouse.position = {0}'.format(mouse.position))
+    print('mouse.release(Button.left)')
+    print('mouse.press(Button.left)')
+    print('time.sleep(.05)')
+    #print("")
+    #print('mouse.position = {0}'.format(mouse.position))
+    time.sleep(1)
+
